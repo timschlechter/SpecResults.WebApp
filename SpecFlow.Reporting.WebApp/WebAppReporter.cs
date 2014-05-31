c7b8b8bdbe5574273aba2a5fdf4e0a4e85b870f6
Tim Schlechter <timschlechter@gmail.com>
2014-05-31 21:58:45 +0200

    Updated packages

--- SpecFlow.Reporting.WebApp/WebAppReporter.cs
+++ SpecFlow.Reporting.WebApp/WebAppReporter.cs
@@ -5,143 +5,143 @@ using System.Text.RegularExpressions;
 
 namespace SpecFlow.Reporting.WebApp
 {
-	public class WebAppReporter : Reporter
-	{
-		protected JsonReporter JsonReporter
-		{
-			get
-			{
-				return new JsonReporter
-				{
-					Report = this.Report
-				};
-			}
-		}
+    public class WebAppReporter : Reporter
+    {
+        protected JsonReporter JsonReporter
+        {
+            get
+            {
+                return new JsonReporter
+                {
+                    Report = this.Report
+                };
+            }
+        }
 
-		public WebAppReporterSettings Settings { get; private set; }
+        public WebAppReporterSettings Settings { get; private set; }
 
-		public WebAppReporter()
-		{
-			Settings = new WebAppReporterSettings();
-		}
+        public WebAppReporter()
+        {
+            Settings = new WebAppReporterSettings();
+        }
 
-		public override void WriteToStream(Stream stream)
-		{
-			throw new System.NotSupportedException("The WebAppReporter only support WriteToFolder()");
-		}
+        public override void WriteToStream(Stream stream)
+        {
+            throw new System.NotSupportedException("The WebAppReporter only support WriteToFolder()");
+        }
 
-		public void WriteToFolder(string folderPath, bool clearDirectory = false)
-		{
-			if (clearDirectory && Directory.Exists(folderPath))
-			{
-				foreach (var dir in Directory.GetDirectories(folderPath))
-				{
-					Directory.Delete(dir, true);
-				}
+        public void WriteToFolder(string folderPath, bool clearDirectory = false)
+        {
+            if (clearDirectory && Directory.Exists(folderPath))
+            {
+                foreach (var dir in Directory.GetDirectories(folderPath))
+                {
+                    Directory.Delete(dir, true);
+                }
 
-				foreach (var file in Directory.GetFiles(folderPath))
-				{
-					File.Delete(file);
-				}
-			}
+                foreach (var file in Directory.GetFiles(folderPath))
+                {
+                    File.Delete(file);
+                }
+            }
 
-			if (!Directory.Exists(folderPath))
-			{
-				Directory.CreateDirectory(folderPath);
-			}
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
 
-			// index.html
-			File.WriteAllText(
-				path: Path.Combine(folderPath, "index.html"),
-				contents: ApplySettings(Resources.index)
-			);
+            // index.html
+            File.WriteAllText(
+                path: Path.Combine(folderPath, "index.html"),
+                contents: ApplySettings(Resources.index)
+            );
 
-			// styles.css
-			File.WriteAllText(
-				path: Path.Combine(folderPath, "styles.css"),
-				contents: Resources.styles
-			);
+            // styles.css
+            File.WriteAllText(
+                path: Path.Combine(folderPath, "styles.css"),
+                contents: Resources.styles
+            );
 
-			// scripts.js
-			File.WriteAllText(
-				path: Path.Combine(folderPath, "scripts.js"),
-				contents: ApplySettings(Resources.scripts)
-			);
+            // scripts.js
+            File.WriteAllText(
+                path: Path.Combine(folderPath, "scripts.js"),
+                contents: ApplySettings(Resources.scripts)
+            );
 
-			// report.js
-			File.WriteAllText(
-				path: Path.Combine(folderPath, "report.js"),
-				contents: string.Format("var report = {0};", JsonReporter.WriteToString())
-			);
+            // report.js
+            File.WriteAllText(
+                path: Path.Combine(folderPath, "report.js"),
+                contents: string.Format("var report = {0};", JsonReporter.WriteToString())
+            );
 
-			var fontsPath = Path.Combine(folderPath, "fonts");
-			if (!Directory.Exists(fontsPath))
-			{
-				Directory.CreateDirectory(fontsPath);
-			}
+            var fontsPath = Path.Combine(folderPath, "fonts");
+            if (!Directory.Exists(fontsPath))
+            {
+                Directory.CreateDirectory(fontsPath);
+            }
 
-			File.WriteAllBytes(
-				path: Path.Combine(fontsPath, "glyphicons-halflings-regular.eot"),
-				bytes: Resources.glyphicons_halflings_regular_eot
-			);
+            File.WriteAllBytes(
+                path: Path.Combine(fontsPath, "glyphicons-halflings-regular.eot"),
+                bytes: Resources.glyphicons_halflings_regular_eot
+            );
 
-			File.WriteAllBytes(
-				path: Path.Combine(fontsPath, "glyphicons-halflings-regular.svg"),
-				bytes: Resources.glyphicons_halflings_regular_svg
-			);
+            File.WriteAllBytes(
+                path: Path.Combine(fontsPath, "glyphicons-halflings-regular.svg"),
+                bytes: Resources.glyphicons_halflings_regular_svg
+            );
 
-			File.WriteAllBytes(
-				path: Path.Combine(fontsPath, "glyphicons-halflings-regular.ttf"),
-				bytes: Resources.glyphicons_halflings_regular_ttf
-			);
+            File.WriteAllBytes(
+                path: Path.Combine(fontsPath, "glyphicons-halflings-regular.ttf"),
+                bytes: Resources.glyphicons_halflings_regular_ttf
+            );
 
-			File.WriteAllBytes(
-				path: Path.Combine(fontsPath, "glyphicons-halflings-regular.woff"),
-				bytes: Resources.glyphicons_halflings_regular_woff
-			);
-		}
+            File.WriteAllBytes(
+                path: Path.Combine(fontsPath, "glyphicons-halflings-regular.woff"),
+                bytes: Resources.glyphicons_halflings_regular_woff
+            );
+        }
 
-		private string ApplySettings(string contents)
-		{
-			contents = contents
-				.Replace("__TITLE__", Settings.GetTitle())
-				.Replace("__VERSION__", Settings.GetVersion());
+        private string ApplySettings(string contents)
+        {
+            contents = contents
+                .Replace("__TITLE__", Settings.GetTitle())
+                .Replace("__VERSION__", Settings.GetVersion());
 
-			// Inject custom css
-			string css = "";
-			if (!string.IsNullOrEmpty(Settings.CssFile))
-			{
-				css = File.ReadAllText(Settings.CssFile);
-			}
-			contents = contents.Replace("__CSS__", css);
+            // Inject custom css
+            string css = "";
+            if (!string.IsNullOrEmpty(Settings.CssFile))
+            {
+                css = File.ReadAllText(Settings.CssFile);
+            }
+            contents = contents.Replace("__CSS__", css);
 
-			// Inject Dashboard text
-			string dashboardText = "";
-			if (!string.IsNullOrEmpty(Settings.DashboardTextFile))
-			{
-				dashboardText = File.ReadAllText(Settings.DashboardTextFile);
-				dashboardText = Markdown.ToHtml(dashboardText);
-				dashboardText = Regex.Replace(dashboardText, "(\r|\n)", "\\n");
-			}
-			contents = contents.Replace("__DASHBOARD_TEXT__", dashboardText);
-			
-			// Inject custom step-details template
-			if (!string.IsNullOrEmpty(Settings.StepDetailsTemplateFile))
-			{
-				var pattern = "(?:<!-- step-details-marker: begin -->)(.*?)(?:<!-- step-details-marker: end -->)";
+            // Inject Dashboard text
+            string dashboardText = "";
+            if (!string.IsNullOrEmpty(Settings.DashboardTextFile))
+            {
+                dashboardText = File.ReadAllText(Settings.DashboardTextFile);
+                dashboardText = Markdown.ToHtml(dashboardText);
+                dashboardText = Regex.Replace(dashboardText, "(\r|\n)", "\\n");
+            }
+            contents = contents.Replace("__DASHBOARD_TEXT__", dashboardText);
 
-				var replacement = File.ReadAllText(Settings.StepDetailsTemplateFile) + "'+\"";
-				replacement = Regex.Replace(replacement, "(\r|\n)", "\\n");
-				replacement = replacement.Replace("'", "\'");
+            // Inject custom step-details template
+            if (!string.IsNullOrEmpty(Settings.StepDetailsTemplateFile))
+            {
+                var pattern = "(?:<!-- step-details-marker: begin -->)(.*?)(?:<!-- step-details-marker: end -->)";
 
-				contents = Regex.Replace(
-					input: contents,
-					pattern: pattern,
-					replacement: replacement
-				);
-			}
+                var replacement = File.ReadAllText(Settings.StepDetailsTemplateFile) + "'+\"";
+                replacement = Regex.Replace(replacement, "(\r|\n)", "\\n");
+                replacement = replacement.Replace("'", "\'");
 
-			return contents;
-		}
-	}
+                contents = Regex.Replace(
+                    input: contents,
+                    pattern: pattern,
+                    replacement: replacement
+                );
+            }
+
+            return contents;
+        }
+    }
 }
--- SpecFlow.Reporting.WebApp/WebAppReporterSettings.cs
+++ SpecFlow.Reporting.WebApp/WebAppReporterSettings.cs
@@ -2,24 +2,24 @@
 
 namespace SpecFlow.Reporting.WebApp
 {
-	public class WebAppReporterSettings
-	{
-		public string Title { get; set; }
+    public class WebAppReporterSettings
+    {
+        public string Title { get; set; }
 
-		internal string GetTitle()
-		{
-			return String.IsNullOrEmpty(Title) ? "{WebAppReporter.Settings.Title}" : Title;
-		}
+        internal string GetTitle()
+        {
+            return String.IsNullOrEmpty(Title) ? "{WebAppReporter.Settings.Title}" : Title;
+        }
 
-		internal string GetVersion()
-		{
-			return Guid.NewGuid().ToString();
-		}
+        internal string GetVersion()
+        {
+            return Guid.NewGuid().ToString();
+        }
 
-		public string StepDetailsTemplateFile { get; set; }
+        public string StepDetailsTemplateFile { get; set; }
 
-		public string CssFile { get; set; }
+        public string CssFile { get; set; }
 
-		public string DashboardTextFile { get; set; }
-	}
+        public string DashboardTextFile { get; set; }
+    }
 }
