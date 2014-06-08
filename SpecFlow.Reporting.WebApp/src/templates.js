447d79a7bff311630fd0ba9578638dd52c9c3f7a
Tim Schlechter <timschlechter@gmail.com>
2014-06-08 20:39:36 +0200

    Added exception info and modal to step template

--- SpecFlow.Reporting.WebApp/src/templates.js
+++ SpecFlow.Reporting.WebApp/src/templates.js
@@ -2,12 +2,25 @@ angular.module('app').run(['$templateCache', function($templateCache) {
   'use strict';
 
   $templateCache.put('directives/result-badge.tpl.html',
-    "<span class=\"label pull-right\" ng-class=\"{ 'label-success': result == 'OK', 'label-danger': result == 'Error', 'label-warning': result == 'Pending', 'label-default': result == 'NotRun' }\">{{result}}</span>"
+    "<span class=\"label pull-right\" ng-class=\"{ \r" +
+    "\n" +
+    "    'label-success': reportItem.result == 'OK', \r" +
+    "\n" +
+    "    'label-danger': reportItem.result == 'Error', \r" +
+    "\n" +
+    "    'label-warning': reportItem.result == 'Pending', \r" +
+    "\n" +
+    "    'label-default': reportItem.result == 'NotRun' }\"><span ng-if=\"!reportItem.exception\">{{reportItem.result}}</span> <span ng-if=\"reportItem.exception\" ng-click=\"showException()\" tooltip=\"{{reportItem.exception.message}}\" tooltip-placement=\"left\" tooltip-append-to-body=\"true\">{{reportItem.result}}</span></span>"
+  );
+
+
+  $templateCache.put('templates/exception-details-modal.tpl.html',
+    "<div class=\"modal-header\"><h3 class=\"modal-title\">Exception details</h3></div><div class=\"modal-body\"><dl class=\"dl-horizontal\"><dt>Message</dt><dd>{{exception.message}}</dd><dt>Type</dt><dd>{{exception.exception_type}}</dd><dt>Source</dt><dd>{{exception.source}}</dd><dt>Stack trace</dt><dd><pre>{{exception.stack_trace}}</pre></dd></dl></div><div class=\"modal-footer ng-scope\"><button class=\"btn btn-primary\" ng-click=\"ok()\">Close</button></div>"
   );
 
 
   $templateCache.put('views/dashboard.tpl.html',
-    "<div ng-controller=\"DashboardController\"><nav class=\"navbar navbar-default navbar-fixed-top\" role=\"navigation\"><div class=\"container\"><!-- Brand and toggle get grouped for better mobile display --><div class=\"navbar-header\"><a class=\"navbar-brand\" href=\"#\">__TITLE__</a></div><div class=\"collapse navbar-collapse\"><form class=\"navbar-form navbar-right\" role=\"search\"><div class=\"form-group\"><input type=\"text\" class=\"form-control\" placeholder=\"Search\"></div></form></div></div></nav><div class=\"container\">__DASHBOARD_TEXT__<h1>Features</h1><accordion close-others=\"oneAtATime\"><accordion-group ng-repeat=\"feature in report.features\" is-open=\"feature.$$isOpen\"><accordion-heading>{{feature.title}} <small class=\"text-muted\"><em>completed in {{feature.duration}} ms</em></small><result-badge result=\"feature.result\"></result-badge></accordion-heading><div ng-bind-html=\"feature.description_html | trusted\"></div><h3>Scenarios</h3><div class=\"list-group\"><a ng-repeat=\"scenario in feature.scenarios\" class=\"list-group-item\" href=\"#/features/{{feature.id|encodeURIComponent}}/scenarios/{{scenario.id|encodeURIComponent}}\">{{scenario.title}} <small class=\"text-muted\"><em>completed in {{scenario.duration}} ms</em></small><result-badge result=\"scenario.result\"></result-badge></a></div></accordion-group></accordion></div></div>"
+    "<div ng-controller=\"DashboardController\"><nav class=\"navbar navbar-default navbar-fixed-top\" role=\"navigation\"><div class=\"container\"><!-- Brand and toggle get grouped for better mobile display --><div class=\"navbar-header\"><a class=\"navbar-brand\" href=\"#\">__TITLE__</a></div><div class=\"collapse navbar-collapse\"><form class=\"navbar-form navbar-right\" role=\"search\"><div class=\"form-group\"><input type=\"text\" class=\"form-control\" placeholder=\"Search\"></div></form></div></div></nav><div class=\"container\">__DASHBOARD_TEXT__<h1>Features</h1><accordion close-others=\"oneAtATime\"><accordion-group ng-repeat=\"feature in report.features\" is-open=\"feature.$$isOpen\"><accordion-heading>{{feature.title}} <small class=\"text-muted\"><em>completed in {{feature.duration}} ms</em></small><result-badge report-item=\"feature\"></result-badge></accordion-heading><div ng-bind-html=\"feature.description_html | trusted\"></div><h3>Scenarios</h3><div class=\"list-group\"><a ng-repeat=\"scenario in feature.scenarios\" class=\"list-group-item\" href=\"#/features/{{feature.id|encodeURIComponent}}/scenarios/{{scenario.id|encodeURIComponent}}\">{{scenario.title}} <small class=\"text-muted\"><em>completed in {{scenario.duration}} ms</em></small><result-badge report-item=\"scenario\"></result-badge></a></div></accordion-group></accordion></div></div>"
   );
 
 
@@ -16,7 +29,7 @@ angular.module('app').run(['$templateCache', function($templateCache) {
     "\n" +
     "\t\t{{step.title}} <small class=\"text-muted\"><em>completed in {{step.duration}} ms</em></small>\r" +
     "\n" +
-    "        <result-badge result=\"step.result\"></result-badge>\r" +
+    "        <result-badge report-item=\"step\"></result-badge>\r" +
     "\n" +
     "\r" +
     "\n" +
