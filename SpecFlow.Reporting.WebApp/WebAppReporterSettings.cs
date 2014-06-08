927bd8791311266d999e086b2c116dff3f9ad495
Tim Schlechter <timschlechter@gmail.com>
2014-06-08 21:05:18 +0200

    Write culture as global var in index.html

--- SpecFlow.Reporting.WebApp/WebAppReporterSettings.cs
+++ SpecFlow.Reporting.WebApp/WebAppReporterSettings.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace SpecFlow.Reporting.WebApp
 {
@@ -16,10 +17,17 @@ namespace SpecFlow.Reporting.WebApp
             return Guid.NewGuid().ToString();
         }
 
+        internal string GetCulture()
+        {
+            return Culture != null ? Culture.Name : "en-US";
+        }
+
         public string StepDetailsTemplateFile { get; set; }
 
         public string CssFile { get; set; }
 
         public string DashboardTextFile { get; set; }
+
+        public CultureInfo Culture { get; set; }        
     }
 }
